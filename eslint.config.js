221f7326fa5c78d784a128448bb3209bab05af2e
bharathttand <bbharath@thetravelagentnextdoor.com>
2026-02-17 19:58:43 +0530

    chore: updated the config

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,22 @@
+import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
 
-export default tseslint.config(
+export default [
   {
-    ignores: ['dist', 'node_modules', 'eslint.config.js', '.husky/*'],
+    ignores: ['dist', 'node_modules', 'eslint.config.js', '.husky/*']
   },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
       parserOptions: {
-        project: ['./tsconfig.json'],
-      },
+        project: ['./tsconfig.json']
+      }
     },
     files: ['**/*.ts'],
-    rules: {},
+    rules: {}
   },
-  prettier,
-);
+  prettier
+];
